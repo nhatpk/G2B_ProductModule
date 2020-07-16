59366e5eb4de3cb5efbb393a4b0af4cfe6670ab0
nhatpk <51379375+nhatpk@users.noreply.github.com>
2020-07-15 18:10:09 -0700

    Initial commit

---
+++ G2B_Product/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="G2B_Product.MvcApplication" Language="C#" %>
---
+++ G2B_Product/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(G2B_Product.Startup))]
+namespace G2B_Product
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
